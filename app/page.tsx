0fe14aea5750fc82a8cdd2c78faae50c6cb0d862
Nerio Balza <neriojbd@gmail.com>
2026-05-18 19:12:34 -0400

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import { FeaturesSection } from "@/src/components/landing/FeaturesSection";
 import { StatsSection } from "@/src/components/landing/StatsSection";
 import { StepsSection } from "@/src/components/landing/StepsSection";
 import { TestimonialSection } from "@/src/components/landing/TestimonialSection";
+import Header from "@/src/components/layout/Header";
 
-export default function LandingPage() {
+export default function HomePage() {
   return (
-    <div className="">
-      <Hero />
-      <FeaturesSection />
-      <StatsSection />
-      <StepsSection />
-      <TestimonialSection />
-      <Footer />
-    </div>
+    <>
+      <Header />
+      <div className="">
+        <Hero />
+        <FeaturesSection />
+        <StatsSection />
+        <StepsSection />
+        <TestimonialSection />
+        <Footer />
+      </div>
+    </>
   );
 }
